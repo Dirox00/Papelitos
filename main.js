0b44c69b126fcbcb1dae3b74b81f5464fc423e65
Dirox00 <rodihackpro@gmail.com>
2020-03-16 19:40:23 +0100

    starting

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
- 
+function clearBox(idName)
+{
+    var elem = document.getElementById(idName);
+    if (elem.style.display === "none") {
+        elem.style.display = "block";
+      } 
+    else {
+        elem.style.display = "none";
+      }
+}
