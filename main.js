954d60e7a7356a326ebd983c8d6e7ea5ed5ea2a1
RoDiHackProg <rodihackpro@gmail.com>
2020-03-18 00:25:44 +0100

    Advanced in frontend

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-users = ['hola'];
-team1 = []
-team2 = []
+var users = ['hola'];
+var team1 = [];
+var team2 = [];
+var words = [];
+var N = 1;
 
 function next(idName)
 {
@@ -10,22 +12,47 @@ function next(idName)
     // alert(elem.nextElementSibling)
 }
 
-login();
+login(); 
 
 function login(){
     username = document.getElementById("name").value;
     if (! users.includes(username)){
         users.push(username);
+        return next('login');
     }
-    return next('login');
+    else{
+        return next('teams');
+    }
 }
 
 function team(n){
-    alert(username)
     if (n == 1){
         team1.push(username);
     }
     else if (n == 2){
         team2.push(username);
+    }
+    return next('teams');
+}
+
+function addWord(){
+    if (word.value != ''){
+        if (words.length < N){
+            word = document.getElementById("word").value;
+            if (! words.includes(word)){
+                words.push(word);
+            }
+            else{
+                alert('Word is already in.');
+            }
+            document.getElementById("ok").innerHTML = word + ' has been added';
+            document.getElementById("word").value = "";
+            // document.getElementById(idName);
+            // $("#words").load(window.location.href + " #words" );
+        }
+        else{
+            alert('Words list is already full.');
+            return next('words');
+        }
     }
 }
